53cfa6682bf2d848f4798e0a535f9cd260de6655
Davi <davi.lucas.7almeida@gmail.com>
2026-06-20 18:09:52 -0300

    fix: create application

---
+++ src/main/java/br/com/davi/trackmyfood/TrackmyfoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.davi.trackmyfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackmyfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackmyfoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/davi/trackmyfood/TrackmyfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.davi.trackmyfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackmyfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
