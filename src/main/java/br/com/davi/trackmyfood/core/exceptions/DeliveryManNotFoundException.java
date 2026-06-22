7b3825ac9132ba384034f5117a8ff9b61a46494f
Davi <davi.lucas.7almeida@gmail.com>
2026-06-22 18:53:38 -0300

    feat: add Exception Not Found DeliveryMan

---
+++ src/main/java/br/com/davi/trackmyfood/core/exceptions/DeliveryManNotFoundException.java
@@ -0,0 +1,12 @@
+package br.com.davi.trackmyfood.core.exceptions;
+
+public class DeliveryManNotFoundException extends NotFoundException {
+
+
+    public DeliveryManNotFoundException(String message) {
+        super(message);
+    }
+    public DeliveryManNotFoundException() {
+        super("deliveryMan not found");
+    }
+}
