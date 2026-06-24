7e0c1417de2d4d72f25a8a2d97d2730871afa758
Davi <davi.lucas.7almeida@gmail.com>
2026-06-24 18:03:24 -0300

    feat: add DeliveryLocationController witch methods registrarLocation and findLocationHistoryByOrder

---
+++ src/main/java/br/com/davi/trackmyfood/core/exceptions/NotFoundException.java
@@ -0,0 +1,7 @@
+package br.com.davi.trackmyfood.core.exceptions;
+
+public class NotFoundException extends AppException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
